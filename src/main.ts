2e53236f1c1d114cc9bbd32efeb4ca00b6ef2964
MarcoJav15 <marcodeleon2002@gmail.com>
2024-05-28 23:31:51 -0600

    Ajustando errores

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const port = process.env.PORT || 3001;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -25,6 +26,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3001);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const dataSource = new DataSource({
     type:'postgres',
-    host: 'localhost', 
+    host: 'dpg-cparfj63e1ms739uhh5g-a.oregon-postgres.render.com', 
     port: 5432, 
-    username: 'url', 
-    password: 'url.2024',
-    database: 'project', 
+    username: 'nest_project_nq2c_user', 
+    password: 'WUFUrDaWTUzqNDr7vo0TiDHIR57WAs1w',
+    database: 'nest_project_nq2c', 
     synchronize: false,
     entities: ['src/**/*.entity.ts'],
     migrations: ['./src/migrations/*.ts'],
