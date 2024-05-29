5276bbfbbcb8c0d4743bed1815c2b72d45ecd7fa
MarcoJav15 <marcodeleon2002@gmail.com>
2024-05-29 00:01:49 -0600

    Cambios

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 const port = process.env.PORT || 3001;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true
-  
-  })
   app.useGlobalPipes(
     new ValidationPipe({
     whitelist: true, 
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const dataSource = new DataSource({
     type:'postgres',
-    host: 'dpg-cparfj63e1ms739uhh5g-a.oregon-postgres.render.com', 
-    port: 5432, 
-    username: 'nest_project_nq2c_user', 
-    password: 'WUFUrDaWTUzqNDr7vo0TiDHIR57WAs1w',
-    database: 'nest_project_nq2c', 
+    host: 'viaduct.proxy.rlwy.net', 
+    port: 32597, 
+    username: 'postgres', 
+    password: 'BCIqcaLjVxZQSFIYkkjxqaFgtPgmVmsv',
+    database: 'railway', 
     synchronize: false,
     entities: ['src/**/*.entity.ts'],
     migrations: ['./src/migrations/*.ts'],
